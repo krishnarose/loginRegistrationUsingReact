4c2e0266369358c57553df51a0fcff29fe1441fb
krishnarose <krishnarose2001@gmail.com>
2024-05-20 19:46:37 +0530

    server setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log('hello')
+import  Express  from "express";
+
+const app = Express();
+
+app.get('/',(req, res)=>{
+    res.send('This is server message')
+})
+
+const PORT = 7000;
+
+app.listen(PORT,()=>{
+    console.log(`Server started on http://localhost:${PORT}`)
+})
